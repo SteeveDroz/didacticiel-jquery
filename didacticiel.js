9d37b5a12ed533ab94283dde5c64aafb53e441b2
Steeve Droz <steeve.droz@rpn.ch>
2016-10-25 12:03:08 +0200

    Récupération du code et application à l'exemple

--- didacticiel.js
+++ didacticiel.js
@@ -7,6 +7,43 @@ $(function() {
     $(this).toggleClass('closed')
   });
 
+  $('.exemple').each(function() {
+    let nom = $(this).data('nom')
+    let elements = []
+    $(this).data('elements').split('|').forEach(function(donnee) {
+      let parties = donnee.split(/#|::/, 3)
+      let balise = parties[0]
+      let attributs = parties[1]
+      let contenu = parties[2]
+      elements.push({
+        attributs: attributs,
+        balise: balise,
+        contenu: contenu
+      })
+    })
+    let code = $(this).find('code').text()
+    let exemple = $('<div>')
+    elements.forEach(function(element) {
+      let attributs = element.attributs.split('&')
+      let nouvelElement = $('<' + element.balise + '>', {
+        id: nom + '_' + attributs.shift(),
+        html: element.contenu
+      })
+      attributs.forEach(function(attribut) {
+        let cleValeur = attribut.split('=')
+        let cle = cleValeur[0]
+        let valeur = cleValeur[1]
+        nouvelElement.attr(cle, valeur)
+      })
+      exemple.append(nouvelElement)
+    })
+
+    $(this).prepend(exemple)
+    $(this).append($('<script></script>', {
+      text: code.replace(/#(\w)/g,'#' + nom + '_$1')
+    }))
+  })
+
   // Les titres ayant la classe "collapsed" sont fermés par défaut.
   $('h2.collapsed, h3.collapsed').click()
 
