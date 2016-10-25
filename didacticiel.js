40ed39022481798eccbcc43dab7271e96571b964
Steeve Droz <steeve.droz@rpn.ch>
2016-10-25 08:50:28 +0200

    Meilleure mise en forme du code js pour l'affichage

--- didacticiel.js
+++ didacticiel.js
@@ -1,11 +1,18 @@
 $(function() {
+  // Lorsqu'on clique sur un titre h2 ou h3...
   $('h2, h3').click(function() {
-    $(this).next('section').slideToggle();
-    $(this).toggleClass('closed');
+    // ... la section suivante s'ouvre ou se ferme...
+    $(this).next('section').slideToggle()
+    // ... et le style du titre se modifie.
+    $(this).toggleClass('closed')
   });
 
-  $('h2.collapsed, h3.collapsed').click();
+  // Les titres ayant la classe "collapsed" sont fermés par défaut.
+  $('h2.collapsed, h3.collapsed').click()
 
+  // On place le numéro de version dans l'élément spécifique.
   $('#jQueryVersion').text($(this).jquery)
+
+  // On insère le code de "didacticiel.js" dans l'élément spécifique.
   $('#jQuerySource').load('didacticiel.js')
-});
+})
